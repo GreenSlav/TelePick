0855bb3283e6c0c4bb44733c0b14af5853bc2d08
Viacheslav <sromanov2608@gmail.com>
2025-02-08 02:19:06 +0300

    Initial Commit

---
+++ TelePick/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TelePick
+//
+//  Created by green_sl on 8.2.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
